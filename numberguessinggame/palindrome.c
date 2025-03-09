2d696d5a6cb1be7b1b4c217dcbf74f2a3e743ebe
suvaa14 <suvaacharya30@gmail.com>
2025-03-09 20:48:38 +0545

    palindrome.c

---
+++ numberguessinggame/palindrome.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_LENGTH 100
+
+// Function to check if a string is a palindrome
+int isPalindrome(char str[]) {
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while (left < right) {
+        // Ignore non-alphanumeric characters
+        while (left < right && !isalnum(str[left])) {
+            left++;
+        }
+        while (left < right && !isalnum(str[right])) {
+            right--;
+        }
+
+        // Compare characters
+        if (tolower(str[left]) != tolower(str[right])) {
+            return 0; // Not a palindrome
+        }
+        left++;
+        right--;
+    }
+    return 1; // Is a palindrome
+}
+
+int main() {
+    char str[MAX_LENGTH];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); // Read a line of text
+
+    // Remove the newline character if present
+    str[strcspn(str, "\n")] = 0;
+
+    if (isPalindrome(str)) {
+        printf("\"%s\" is a palindrome.\n", str);
+    } else {
+        printf("\"%s\" is not a palindrome.\n", str);
+    }
+
+    return 0;
+}
